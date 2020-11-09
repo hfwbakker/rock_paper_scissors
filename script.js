74a3ea8cebd059e7011261b92b0acb8cc6d271a8
Henri Bakker <hfwbakker@gmail.com>
2020-11-09 20:08:56 +0800

    changed from while loop to for loop, best of 5 works now. project finished

--- script.js
+++ script.js
@@ -2,83 +2,62 @@ let compScore = 0
 let playerScore = 0
 
 function computerPlay() {
-    let options = ["rock", "paper", "scissors"]
-    console.log("computerPlay() is executing.")
+    let options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * options.length)];
 }
 
 function playRound() {
-    console.log("playRound() is executing.")
-    return prompt("Type rock, paper, or scissors.").toLowerCase();
+    return prompt("Type rock, paper, or scissors.").toLowerCase()
 }
-
-// let computerSelection = computerPlay();
-// let playerSelection = playRound();
-
 
 function fuknPlay(a, b) {
     if(a == b) {
-        console.log("draw")
-        // game()
+        console.log("draw");
     } else if (a == "rock") {
         if(b == "paper") {
-            console.log("Computer wins.")
+            console.log("Computer wins.");
             compScore += 1
-            // game()
         } else {
-            console.log("Player wins")
+            console.log("Player wins");
             playerScore += 1
-            // game()
         }
     } else if(a == "paper") {
         if(b == "scissors") {
-            console.log("Computer wins")
+            console.log("Computer wins");
             compScore += 1
-            // game()
         } else {
-            console.log("Player wins")
+            console.log("Player wins");
             playerScore += 1
-            // game()
         }
     } else if(a == "scissors") {
         if(b == "rock") {
             console.log("Computer wins")
             compScore += 1
-            // game()
         } else {
-            console.log("Player wins")
+            console.log("Player wins");
             playerScore += 1
         }
     } else {
-        console.log("....wtf did you input?")
-        // game()
+        console.log("....wtf did you input?");
     }
 }
 
-function quitIt() {
-    console.log("Game Over");
-    return false;
-}
-
-// function checkWin() {
-//     if(playerScore == 5) {
-//         console.log("You win!")
-//         quitIt();
-//     } else if(compScore == 5) {
-//         console.log("Computer wins")
-//         quitIt();
-//     }
-// }
-
 function game() {
-    // checkWin()
-    console.log(`Welcome. Player score is ${playerScore}, computer score is ${compScore}.`)
     let computerSelection = computerPlay();
     let playerSelection = playRound();
     fuknPlay(playerSelection, computerSelection);
 }
 
-while(playerScore != 5 | compScore == 5){
-    console.log("were ih the whiiiile loop inspectah!")
+for (let i = 1; i <= 5; i++){
+    console.log(`Round ${i}`)
+    console.log(`Player score is ${playerScore}, computer score is ${compScore}.`)
     game();
+}
+
+if (compScore > playerScore){
+    alert("comp wins");
+} else if (playerScore > compScore){
+    alert("player wins")
+} else {
+    console.log("draw")
 }
